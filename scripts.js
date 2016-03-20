9cf89fdff4411966cd452473eba171c336322244
karitomar <krito3791@gmail.com>
2016-03-20 15:39:02 -0500

    Correcion de errores en los Dos archivos

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 console.log("Inicio de programa");
-a("Hola JavaScript"
-document.write("<h1>Estamos programando en JavaScript</h1>");
+alert("Hola JavaScript");
+document.write("<h1>Estamos programando en JavaScript</p>");
 alert("Gracias por visitarnos");
 console.log("Fin de programa");
