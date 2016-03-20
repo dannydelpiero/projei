63a4ffcdbbebe0e9718a938263ee83e3a611fc3c
Danny Del Pezo <ninedelpezo@gmail.com>
2016-03-20 15:12:01 -0500

    Creamos java script

---
+++ scripts.js
@@ -0,0 +1,5 @@
+console.log("Inicio de programa");
+alert("Hola JavaScript");
+document.write("<h1>Estamos programando en JavaScript</h1>");
+alert("Gracias por visitarnos");
+console.log("Fin de programa");
